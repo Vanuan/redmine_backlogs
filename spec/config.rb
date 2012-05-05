b098665cd788f990487ce347eca56eda3330a1bf
John Yani <vanuan@gmail.com>
2012-05-06 00:30:41 +0300

    Coverage for rspec

--- spec/config.rb
+++ spec/config.rb
@@ -2,3 +2,10 @@ RSpec.configure do |config|
   # some (optional) config here
   config.mock_with :rspec
 end
+if RUBY_VERSION >= "1.9"
+  require 'simplecov'
+  SimpleCov.start 'rails' do
+    add_group('Backlogs', 'redmine_backlogs')
+  end
+end
+
