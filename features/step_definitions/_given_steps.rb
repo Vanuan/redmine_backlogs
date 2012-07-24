efd04bf73815039a5ca5cfc6d3fc17c3f7db5d9a
John Yani <vanuan@gmail.com>
2012-07-25 01:33:31 +0300

    Seed the database only once

--- features/step_definitions/_given_steps.rb
+++ features/step_definitions/_given_steps.rb
@@ -194,7 +194,7 @@ Given /^the (.*) project has the backlogs plugin enabled$/ do |project_id|
   @project.update_attribute :tracker_ids, (story_trackers << task_tracker)
 
   # make sure existing stories don't occupy positions that the tests are going to use
-  Issue.connection.execute("update issues set position = (position - #{Issue.minimum(:position)}) + #{Issue.maximum(:position)} + 50000")
+  # Issue.connection.execute("update issues set position = (position - #{Issue.minimum(:position)}) + #{Issue.maximum(:position)} + 50000")
 end
 
 Given /^no versions or issues exist$/ do
